1b7cab226063814a106b519745e3dabde2ba97d5
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-10-17 15:15:09 +0900

    commit

---
+++ customercenter/src/main/java/labshopcqrs/infra/MyPageRepository.java
@@ -0,0 +1,10 @@
+package labshopcqrs.infra;
+
+import java.util.List;
+import labshopcqrs.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "myPages", path = "myPages")
+public interface MyPageRepository
+    extends PagingAndSortingRepository<MyPage, Long> {}
